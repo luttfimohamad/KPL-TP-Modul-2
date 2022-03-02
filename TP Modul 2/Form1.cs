d8029080081aa305a728d499c96e4d44a260da9d
luttfimohamad <luttfimohamad@gmail.com>
2022-03-02 21:57:47 +0700

    Menambahkan  GUI

---
+++ TP Modul 2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_Modul_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nah = textBox1.Text;
+
+            label1.Text = "Halo " + nah + "!";
+
+
+        }
+
+    }
+}
